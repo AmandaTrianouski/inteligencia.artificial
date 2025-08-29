428d88c3945669f7d87a59626664dfbc4b3f20ea
AmandaTrianouski <160858147+AmandaTrianouski@users.noreply.github.com>
2025-08-29 08:25:41 -0300

    nmbnm

--- script.js
+++ script.js
@@ -22,31 +22,47 @@ const perguntas = [
     },
     {
        
-            enunciado: "As fontes renováveis de energia são aquelas formas de produção de energia em que suas fontes são capazes de manter-se disponíveis durante um longo prazo, contando com recursos que se regeneram ou que se mantêm ativos permanentemente. Com base nisso, temos a energia geotérmica, que consiste em utilizar o calor manifestado em áreas próximas à superfície. Qual a sua opinião sobre esse modelo de energia renovável?",
+            enunciado: "Você costuma assistir animes durante o dia ou a noite?",
             alternativas: [
                 {
-                    texto: "A energia geotérmica precisa ser mais explorada visto que utiliza o calor interno da terra, pois não depende do clima e é inesgotável.",
-                    afirmacao: "Você demonstra um perfil  de pesquisador e inovador que busca por meio da inovação soluções para o meio ambiente e sustentabilidade das gerações posteriores."
+                    texto: "Durante o dia",
+                    afirmacao: "Você é uma raposa como o Naruto, proavelmente é extrovertido/a e tem tempo de sobra,"
                 },
                 {
-                    texto:  "Seria interessante explorar primeiramente fontes renováveis exotérmicas, em segundo plano utilizar a energia geotérmica apesar dos custos elevados de explorações e possibilidades de contaminação de rios. ",
-                    afirmacao: "Você é uma pessoa que dá ênfase em priorizar os recursos disponíveis, pensando no meio ambiente e sem descartar o viés econômico."
+                    texto:  "Durante a noite",
+                    afirmacao: "Você é um/a coruja da noite, proavelmente é introvertido/a e tem muitos afazere durante o dia,"
                 }    
                
             ]
         },
         {
-            enunciado: "Hoje em dia é cada vez mais importante conhecer o seu corpo, suas qualidades, suas limitações e entender suas emoções. Saber controlá-las é o desafio das pessoas. A sua auto-estima impacta a sua qualidade de vida?",
+            enunciado: "Você é do lado o herói ou do vilão?",
 
             alternativas: [
                 {
-                    texto: "Na maior parte do tempo, com certeza impacta, pois é a partir da autoestima que temos um olhar positivo ou negativo sobre nossas ações diárias. Sempre estou em busca de mais autoconhecimento.",
-                    afirmacao: "Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo."
+                    texto: "Herói",
+                    afirmacao: "Tenho certeza que você é uma pessoa justa, certa e odeia injustiças."
                 },
                 {
-                    texto:    "Apenas ocasionalmente ou raramente. Às vezes minha autoestima oscila e isso pode afetar negativamente minha qualidade de vida em certas ocasiões.",
+                    texto:  "Vilão",
                
-                    afirmacao: "Você demomostra que precisa se conhecer mais e mostra uma consciência de uma crescente necessidade de cuidar mais da própria percepção e bem-estar emocional."
+                    afirmacao: "Bom... de duas uma, ou você se identifica com o vilão ou você é do contra mesmo."
+                }    
+               
+            ]
+        },
+        {
+            enunciado: "Que estilo de anime você mais gosta?",
+
+            alternativas: [
+                {
+                    texto: "Suspense, aventura e ação",
+                    afirmacao: "Você é uma pessoa de personalidade forte e de bom gosto."
+                },
+                {
+                    texto:  "Romance e cotidiano",
+               
+                    afirmacao: "Você é uma pessoa fofa ou... alguém que sonha em vier o que assite no anime."
                 }    
                
             ]
